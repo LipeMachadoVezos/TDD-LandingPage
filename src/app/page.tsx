2ff4cb5bb4d61e4cb14cf6e6eaadaddff7502498
LipeMachadoVezos <felipe.machado@vezos.com.br>
2024-03-11 17:25:04 -0300

    Add Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,20 @@ export default function Home() {
   return (
     <>
       <header className="flex justify-center items-center w-full h-screen bg-header gap-10">
-        <div className="border-2 border-red-600 max-w-[30%] text-[#F1A632]">
-          <h2>
+        <div className="max-w-[38%] text-[#F1A632]">
+          <h1 className="font-semibold text-[40px] mb-10">
             Faça com que seus professores e educadores públicos alcancem um nível
             de motivação e desempenho jamais atingido.
-          </h2>
+          </h1>
+          <div className="flex flex-col gap-4">
+            <h2 className="text-white text-2xl">Confira um trecho do nosso material.</h2>
+            <input className="py-3 px-2 rounded focus:outline-none focus:ring-2 ring-[#F1A632] ring-inset text-black" type="text" placeholder="Seu nome:" />
+            <input className="py-3 px-2 rounded focus:outline-none focus:ring-2 ring-[#F1A632] ring-inset text-black" type="email" placeholder="Seu melhor e-mail:" />
+            <button className="bg-[#F1A632] hover:bg-[#F1A632]/80 text-white py-3 rounded">Fazer download gratuitamente</button>
+          </div>
         </div>
         <div>
-          <Image src={'/imageHeader.png'} width={760} height={620} alt="" />
+          <Image src={'/imageHeader.png'} width={600} height={0} alt="" />
         </div>
       </header>
     </>
