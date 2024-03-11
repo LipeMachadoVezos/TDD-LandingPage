653847074b15c8058aede6d659f2da29f316f9c3
LipeMachadoVezos <felipe.machado@vezos.com.br>
2024-03-11 16:36:18 -0300

    Add Git Ignore and Header Image and Text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,17 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="w-full h-screen bg-header"></div>
+      <header className="flex justify-center items-center w-full h-screen bg-header gap-10">
+        <div className="border-2 border-red-600 max-w-[30%] text-[#F1A632]">
+          <h2>
+            Faça com que seus professores e educadores públicos alcancem um nível
+            de motivação e desempenho jamais atingido.
+          </h2>
+        </div>
+        <div>
+          <Image src={'/imageHeader.png'} width={760} height={620} alt="" />
+        </div>
+      </header>
     </>
   );
 }
