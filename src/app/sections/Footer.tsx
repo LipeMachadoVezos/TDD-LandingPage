a0bd6b9aa4193cbed983006389058964a7071045
LipeMachado <LipeMachadoVezos>
2024-03-18 17:21:38 -0300

    Change image position

--- src/app/sections/Footer.tsx
+++ src/app/sections/Footer.tsx
@@ -8,7 +8,7 @@ export function Footer() {
     return (
         <>
             <section className="relative flex justify-center items-top w-full h-[50vh] backdrop-blur-md bg-footer bg-cover bg-right sm:bg-center gap-8 px-4 py-16">
-                <Image className="absolute -top-80 right-0" src={'/MapFooter.png'} width={500} height={0} alt="" />
+                <Image className="hidden md:block w-[50%] md:w-[30%] lg:w-[25%] xl:w-[30%] absolute -top-40 lg:-top-80 right-0" src={'/MapFooter.png'} width={500} height={0} alt="" />
                 <div className="flex flex-col justify-center items-center gap-10">
                     <motion.div
                         initial={{ opacity: 0 }}
@@ -22,16 +22,16 @@ export function Footer() {
                             contate-nos aqui:
                         </h1>
                     </motion.div>
-                    <div className="flex justify-center items-center gap-8">
+                    <div className="flex-col flex sm:flex-row justify-center items-center gap-8 w-full">
                         <Button
                             link=""
-                            className="bg-[#AE2484] hover:bg-[#db33a8] px-24"
+                            className="bg-[#AE2484] hover:bg-[#db33a8] w-full"
                         >
                             Whatsapp
                         </Button>
                         <Button
                             link=""
-                            className="bg-[#AE2484] hover:bg-[#db33a8] px-24"
+                            className="bg-[#AE2484] hover:bg-[#db33a8] w-full"
                         >
                             E-email
                         </Button>
